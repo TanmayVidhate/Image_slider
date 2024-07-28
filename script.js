c1ad426dc0afa18cc145e65e663b5c872413b6f7
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-07-28 20:44:30 +0530

    two function created

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const img = [
     "./image/img2.jpg",
     "./image/img3.jpg"
 ];
+
+let imgIndex = 0;
+
+const image = document.getElementById('image')
+function perv() {
+    (imgIndex === 0)?imgIndex = img.length-1:imgIndex--;
+    
+    image.src=img[imgIndex]
+}
+
+function next(){
+    (imgIndex === img.length-1)?imgIndex =0:imgIndex++;
+    
+    image.src=img[imgIndex]
+}
